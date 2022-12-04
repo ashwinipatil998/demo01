e80682e6cb5cdd841c568f4f71c29a7ba0d5ea84
sujit yadav <sujityadav99@gmail.com>
2022-12-04 17:26:54 +0530

    added code for subtraction

--- addition.py
+++ addition.py
@@ -1,4 +1,8 @@
 def add(a,b):
     return a+b
+
+def subtract(a,b):
+    return a-b
 print('the addition is: ',add(5,6))
 print('the addition is: ',add(7,6))
+print('the subtraction is: ',subtract(17,6))
